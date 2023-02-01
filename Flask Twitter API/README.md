5cccb1d757fe4afcc4d3f4ea46dfb778098debc0
AustinSansing <113549451+AustinSansing@users.noreply.github.com>
2023-02-01 16:18:42 -0600

    Add files via upload

---
+++ Flask Twitter API/README.md
@@ -0,0 +1,20 @@
+# Tweepy-Flask
+Flask backend and Twitter API Library (Tweepy)
+This python script displays the tweets made by the entered twitter handle or desired hashtag on your browser.
+Furthermore, you can also enter a sentence and get its overall emotion (sentiment analysis) using python's TextBlob library.
+I further intend to implement sentiment analysis of tweets.
+
+Features:
+---------------------------------------------------------------------------------------------
+* You can get tweets by desired username
+* You can get tweets by desired hashtag
+* You can get tweets which contain a particular word or phrase
+* No need of loggin in
+* Get the sentiment of the desired sentence in just one click!
+---------------------------------------------------------------------------------------------
+
+To run it the project in Pycharm:
+---------------------------------------------------------------------------------------------
+Follow the instructions in the API Lecture pdf to set up a project in the Twitter developer console.
+
+Get the keys and tokens and paste them in the myapp.py file where requested.
---
+++ Flask Twitter API/myapp.py
@@ -0,0 +1,73 @@
+from flask import Flask, render_template, request, redirect, url_for, session, g, flash
+import tweepy 
+import requests
+from textblob import TextBlob
+
+
+############################# TWEEPY SETUP  ######################################
+
+consumer_key = '<paste the key that you created in the twitter developer console>'
+consumer_secret = '<paste the secret key that you created in the twitter developer console>'
+access_token = '<paste the access token that you created in the twitter developer console>'
+access_token_secret = '<paste the access token secret that you created in the twitter developer console>'
+
+auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+auth.set_access_token(access_token, access_token_secret)
+api = tweepy.API(auth)
+##################################################################################
+
+app = Flask(__name__) 
+ 
+@app.route('/')
+def page():
+    return render_template('home.html')
+ 
+
+#---------------T W I T T E R  A P I---------------
+#---------------GETTING TWEET SPECIFIC TO USERNAME---------------
+@app.route('/send', methods=['GET', 'POST'])
+def send():
+	if request.method=='POST':
+		user_interest = request.form['user_interest']
+		print(user_interest)
+		usr = api.get_user(screen_name=user_interest)
+		a = usr.followers_count
+
+		public_tweets = api.user_timeline(screen_name=user_interest, count=100, include_rts=True, exclude_replies=True)
+
+
+		return render_template('send.html', user_interest = user_interest, a = a, public_tweets = public_tweets)
+
+	return render_template('home.html')
+
+#---------------GETTING TWEETS SPECIFIC TO HASHTAG---------------
+@app.route('/hash', methods=['GET', 'POST'])
+def hash():
+	if request.method=='POST':
+		hashtag = request.form['hashtag']
+		h = tweepy.Cursor(api.search_tweets, q = hashtag, count = "10", lang = "en", since = "2018-11-11").items()
+		return render_template('hash.html', h = h, hashtag = hashtag)
+
+	return render_template('map.html')
+
+#---------------GETTING TWEETS FOR SENTIMENT ANALYSIS---------------
+@app.route('/senti', methods=['GET', 'POST'])
+def senti():
+	if request.method=='POST':
+		word = request.form['senti_word']
+		#pt = api.search(word)
+		analysis = TextBlob(word)
+		sentiment = analysis.sentiment.polarity
+		if sentiment > 0:
+			ans = 'Positive Statement'
+		elif sentiment < 0:
+			ans = 'Negative Statement'
+		else:
+			ans = 'Neutral Statement'
+		return render_template('senti.html', word = word, ans = ans, sentiment = sentiment)
+	return render_template('senti_form.html')
+
+
+if (__name__ == "__main__"):
+	app.debug = True
+	app.run(threaded = True)
